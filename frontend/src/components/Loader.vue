911dd77b67e1b89e31ba2e7d96ca0853e662a19f
gunn <gunnkeem@icloud.com>
2020-09-15 19:50:19 +0900

    created: Loader.vue

---
+++ frontend/src/components/Loader.vue
@@ -0,0 +1,54 @@
+<template>
+<div id="loader">
+
+
+
+
+</div>
+</template>
+
+
+
+<script>
+
+
+export default {
+  name: "Loader",
+  components: { },
+  props: [
+
+  ],
+  data() { return {
+
+  }},
+  computed: {
+
+  },
+  methods: {
+
+  },
+  created() {
+
+  },
+  mounted() {
+    
+  },
+  beforeUpdate() {
+    
+  },
+  beforeCreate() {
+    
+  },
+}
+</script>
+
+
+
+<style lang="scss" scoped> 
+
+
+
+
+
+
+</style>
